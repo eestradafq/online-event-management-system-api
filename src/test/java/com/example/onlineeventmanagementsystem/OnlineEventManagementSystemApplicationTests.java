15ae406f3232f34e00c718b6a1d22fa1537f09fc
Erick Estrada <erickestradapj@gmail.com>
2025-01-27 19:36:52 -0500

    initial commit

---
+++ src/test/java/com/example/onlineeventmanagementsystem/OnlineEventManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineeventmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineEventManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
